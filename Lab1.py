e2fde798fe9abed694d76eeffe37dabdfada87b1
frahma1 <frahma1@ualberta.ca>
2023-09-11 17:53:23 -0600

    step 11

--- Lab1.py
+++ Lab1.py
@@ -5,3 +5,8 @@ print(requests.__version__)
 
 page = requests.get('https://www.google.com/')
 
+url = "https://raw.githubusercontent.com/frahma1/CMPUT-404--Lab-1/master/Lab1.py"
+
+res = requests.get(url)
+
+print(res.text)
